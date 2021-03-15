bec38474291316b9fc142f71fc4d2c6b148a8f0b
Dzevad <ynwa886@gmail.com>
2021-03-16 00:55:03 +0100

    Use the parseInt Function with a Redix

---
+++ ex15/src/index.js
@@ -0,0 +1,11 @@
+//Only change code below this line
+function convertToInt(str) {
+    return parseInt(str, 16);
+}
+//Only change code above this line
+convertToInt("BA");
+console.log(convertToInt("BA"));
+console.log(convertToInt("F1"));
+console.log(convertToInt("JeffBezos"));
+
+module.exports = convertToInt;
