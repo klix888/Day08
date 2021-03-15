518d98fd28755882388cdc2f757e4e1d2c5d6bad
Dzevad <ynwa886@gmail.com>
2021-03-16 00:40:25 +0100

    Function - parseInt()

---
+++ ex14/src/index.js
@@ -0,0 +1,11 @@
+//Only change code below this line
+function convertStrToInt(str) {
+    return parseInt(str);
+}
+//Only change code above this line
+convertStrToInt("44");
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+
+module.exports = convertStrToInt;
